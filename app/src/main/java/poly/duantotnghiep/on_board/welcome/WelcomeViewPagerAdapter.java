6734b476425d5a8810e18c133ee5c013ce6c933b
hungcutephomaiqueconbocuoi <hung0868358175@gmail.com>
2023-02-24 21:51:30 +0700

    make onboard and welcome interface

---
+++ app/src/main/java/poly/duantotnghiep/on_board/welcome/WelcomeViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package poly.duantotnghiep.on_board.welcome;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class WelcomeViewPagerAdapter extends FragmentStateAdapter {
+
+    public WelcomeViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new Welcome1Fragment();
+            case 1:
+                return new Wellcome2Fragment();
+            case 2:
+                return new Wellcome3Fragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
